1129c32b90a265ececbeb97b88214a62ed0dd192
shija <khajshija@gmail.com>
2023-11-28 01:35:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+function click1()  {
+
+var audio = new Audio("sounds/crash.mp3")
+audio.play();
+}
+
+function click2()  {
+
+    var audio = new Audio("sounds/kick-bass.mp3")
+    audio.play();
+    }
+
+    function click3()  {
+
+        var audio = new Audio("sounds/snare.mp3")
+        audio.play();
+        }
+
+        function click4()  {
+
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            }
+
+            function click5()  {
+
+                var audio = new Audio("sounds/tom-2.mp3")
+                audio.play();
+                }
+
+                function click6()  {
+
+                    var audio = new Audio("sounds/tom-3.mp3")
+                    audio.play();
+                    }
+                    
+                              function click7()  {
+
+                        var audio = new Audio("sounds/tom-4.mp3")
+                        audio.play();
+                        }
+                        
